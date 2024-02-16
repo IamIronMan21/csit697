530d8dc2c23f7e992088c6d5a587edf4a871ac55
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-02-16 00:13:20 -0500

    Update index.php

--- courses/index.php
+++ courses/index.php
@@ -121,8 +121,8 @@ $courses = $stmt->fetchAll();
         </div>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
-          <button type="button" class="text-sm font-semibold leading-6 text-gray-900" id="js-close">Back</button>
-          <button type="submit" name="new-course-button" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+          <button type="button" class="text-sm font-semibold leading-6 text-gray-900" id="js-close">Cancel</button>
+          <button type="submit" name="new-course-button" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add</button>
         </div>
       </form>
     </dialog>
