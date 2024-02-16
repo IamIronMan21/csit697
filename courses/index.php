f5a7f99b400bd269e7f9eac3d39ce07bf5527831
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-02-15 21:47:23 -0500

    Update index.php

--- courses/index.php
+++ courses/index.php
@@ -102,13 +102,27 @@ $courses = $stmt->fetchAll();
       </table>
     </div>
 
-    <dialog class="w-1/2 h-1/2 rounded-md backdrop:backdrop-brightness-75" id="dialog">
-      <h1>Add a new course</h1>
-      <form class="w-1/2 h-1/2 mx-auto mt-10" method="post">
-        <input class="border" type="text" name="course-name" placeholder="New course name">
-        <input type="submit" name="new-course-button" value="Add">
-        <div>
-          <button type="button" id="js-close">Close</button>
+    <dialog class="w-1/2 rounded-xl backdrop:backdrop-brightness-[65%]" id="dialog">
+      <form method="post" class="w-4/5 mx-auto py-8">
+        <div class="space-y-10">
+          <div class="border-b border-gray-900/10 pb-12">
+            <h2 class="text-base font-semibold leading-7 text-gray-900">New Course</h2>
+            <p class="mt-1 text-sm leading-6 text-gray-600">Add a new course here.</p>
+
+            <div class="mt-8 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div class="sm:col-span-4">
+                <label class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                <div class="mt-2">
+                  <input name="course-name" type="text" class="block px-2.5 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+          <button type="button" class="text-sm font-semibold leading-6 text-gray-900" id="js-close">Back</button>
+          <button type="submit" name="new-course-button" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
         </div>
       </form>
     </dialog>
