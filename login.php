b7b8b6050010a2c759d4279356a05fc6fbe7ab7b
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-02-18 10:04:52 -0500

    Update login.php

--- login.php
+++ login.php
@@ -33,12 +33,16 @@ if (isset($_POST["submit-button"])) {
 
   <title>Quizify</title>
 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Literata:ital,opsz@0,7..72;1,7..72&display=swap">
+
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="min-h-screen">
+<body class="min-h-screen bg-gray-00">
 
-  <header class="px-8 py-5 bg-gray-800">
+  <header class="px-8 py-2.5 bg-gray-00">
     <div class="flex items-center">
       <div class="grow">
         &nbsp;
@@ -46,8 +50,9 @@ if (isset($_POST["submit-button"])) {
     </div>
   </header>
 
-  <div class="w-1/2 mx-auto bg-white min-h-screen px-8">
-    <h1 class="my-4">Login</h1>
+  <div class="w-[447.5px] mx-auto bg-white min pt-5 pb-10 px-8 border border-slate-300 rounded-lg">
+    <h1 class="font-['Literata'] text-2xl my-4 text-center">Quizify</h1>
+    <p class="text-center text-xl font-semibold">Sign in</p>
 
     <form method="post" class="mb-4">
 
@@ -55,14 +60,37 @@ if (isset($_POST["submit-button"])) {
         <p class="text-red-500"><?= $error_message ?></p>
       <?php endif; ?>
 
-      <input class="border border-slate-400" type="text" name="email" placeholder="Email" required>
+      <div>
+        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+        <div class="mt-2">
+          <input id="email" name="email" type="text" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+      </div>
 
-      <input class="border border-slate-400" type="password" name="password" placeholder="Password" required>
+      <!-- <input class="border border-slate-400" type="text" name="email" placeholder="Email" required> -->
 
-      <input class="border bg-slate-200" type="submit" name="submit-button" value="Submit">
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+        </div>
+        <div class="mt-2">
+          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+      </div>
+
+      <!-- <input class="border border-slate-400" type="password" name="password" placeholder="Password" required> -->
+
+      <div class="my-10">
+        Need an account? <a href="./register.php" class="text-indigo-600 underline">Register here</a>
+      </div>
+
+      <div class="my-10">
+
+        <a href="./" class="text-blue-500 underline">Back</a>
+        <input class="border bg-slate-200" type="submit" name="submit-button" value="Submit">
+      </div>
     </form>
 
-    <a href="./" class="text-blue-500 underline">Back</a>
   </div>
 </body>
 
