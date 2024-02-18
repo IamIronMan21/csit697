d41f6f3e187b0b923d14e537827acef2bb5df9c6
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-02-18 10:22:13 -0500

    Update register.php

--- register.php
+++ register.php
@@ -34,12 +34,16 @@ if (isset($_POST["submit-button"])) {
 
   <title>Quizify</title>
 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Literata:ital,opsz@0,7..72;1,7..72&display=swap">
+
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="min-h-screen">
+<body class="min-h-screen bg-gray-00">
 
-  <header class="px-8 py-5 bg-gray-800">
+  <header class="px-8 py-2.5 bg-gray-00">
     <div class="flex items-center">
       <div class="grow">
         &nbsp;
@@ -47,8 +51,9 @@ if (isset($_POST["submit-button"])) {
     </div>
   </header>
 
-  <div class="w-1/2 mx-auto bg-white min-h-screen px-8">
-    <h1 class="my-4">Register</h1>
+  <div class="w-[447.5px] mx-auto bg-white min pt-5 pb-10 px-8 border border-slate-300 rounded-lg">
+    <h1 class="font-['Literata'] text-2xl my-4 text-center">Quizify</h1>
+    <p class="text-center text-xl font-semibold">Register</p>
 
     <?php if (isset($error_message)) : ?>
       <p class="text-red-500"><?= $error_message ?></p>
@@ -64,10 +69,17 @@ if (isset($_POST["submit-button"])) {
       <label for="password">Password</label>
       <input class="border border-slate-400" type="password" name="password" required><br>
 
-      <input class="border bg-slate-200" name="submit-button" type="submit" value="Submit">
-    </form>
 
-    <a href="./" class="text-blue-500 underline">Back</a>
+
+      <div class="my-10 flex">
+
+        <a href="./" class="text-blue-500 underline">Back</a>
+        <input class="border bg-slate-200" type="submit" name="submit-button" value="Submit">
+      </div>
+      <div class="my-10">
+        Already have an account? <a href="./login.php" class="text-indigo-600 underline">Sign in</a>
+      </div>
+    </form>
   </div>
 </body>
 
