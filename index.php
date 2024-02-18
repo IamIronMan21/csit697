6de42e649b0ea73f869518152157522b34c1f00c
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-02-18 10:21:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -30,12 +30,16 @@ if (isset($_POST["submit-button"])) {
 
   <title>Quizify</title>
 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Literata:ital,opsz@0,7..72;1,7..72&display=swap">
+
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="min-h-screen">
+<body class="min-h-screen bg-gray-00">
 
-  <header class="px-8 py-5 bg-gray-800">
+  <header class="px-8 py-2.5 bg-gray-00">
     <div class="flex items-center">
       <div class="grow">
         &nbsp;
@@ -43,8 +47,8 @@ if (isset($_POST["submit-button"])) {
     </div>
   </header>
 
-  <div class="w-1/2 mx-auto bg-white min-h-screen px-8">
-    <h1 class="my-4">Quizify</h1>
+  <div class="w-[447.5px] mx-auto bg-white min pt-5 pb-10 px-8 border border-slate-300 rounded-lg">
+    <h1 class="font-['Literata'] text-2xl my-4 text-center">Quizify</h1>
 
     <?php if (isset($error_message)) : ?>
       <p class="text-red-500"><?= $error_message ?></p>
