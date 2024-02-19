39b77de8b8b69dfa584ffc518616d9aa6321cb82
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-02-19 08:30:57 -0500

    Update login.php

--- login.php
+++ login.php
@@ -40,17 +40,8 @@ if (isset($_POST["submit-button"])) {
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="min-h-screen bg-gray-00">
-
-  <header class="px-8 py-2.5 bg-gray-00">
-    <div class="flex items-center">
-      <div class="grow">
-        &nbsp;
-      </div>
-    </div>
-  </header>
-
-  <div class="w-[447.5px] mx-auto bg-white min pt-5 pb-10 px-8 border border-slate-300 rounded-lg">
+<body>
+  <div class="w-[447.5px] mx-auto bg-white pt-5 pb-10 px-8 border border-slate-300 rounded-lg text-center mt-11">
     <h1 class="font-['Literata'] text-2xl my-4 text-center">Quizify</h1>
     <p class="text-center text-xl font-semibold">Sign in</p>
 
@@ -61,13 +52,13 @@ if (isset($_POST["submit-button"])) {
       <?php endif; ?>
 
       <div>
-        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+        <div class="flex items-center justify-between">
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+        </div>
         <div class="mt-2">
           <input id="email" name="email" type="text" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
         </div>
       </div>
-
-      <!-- <input class="border border-slate-400" type="text" name="email" placeholder="Email" required> -->
 
       <div>
         <div class="flex items-center justify-between">
@@ -78,11 +69,12 @@ if (isset($_POST["submit-button"])) {
         </div>
       </div>
 
-      <!-- <input class="border border-slate-400" type="password" name="password" placeholder="Password" required> -->
-
       <div class="my-8 flex">
         <input type="submit" name="submit-button" value="Submit" class="rounded-md w-full bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
       </div>
+
+      <hr class="w-4/5 mx-auto">
+
       <div class="text-center">
         <div class="my-5">
           Don't have an account? <a href="./register.php" class="text-indigo-600 underline">Sign up</a>
