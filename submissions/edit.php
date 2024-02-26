f70712399fee3536cafa3ac4f2974c2785aec8f8
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-02-26 11:29:21 -0500

    Update edit.php

--- submissions/edit.php
+++ submissions/edit.php
@@ -4,6 +4,46 @@ require "../utils.php";
 
 session_start();
 
+$submission_id = $_GET["submission_id"];
+
+// echo $submission_id;
+
+$sql = "SELECT * FROM submissions WHERE id = ? LIMIT 1";
+$stmt = prepare_and_execute($sql, [$submission_id]);
+$submission = $stmt->fetch();
+
+$sql = "
+SELECT
+  q.id AS id,
+  q.content AS question,
+  GROUP_CONCAT(c.content SEPARATOR '|') AS choices,
+  r.content AS response,
+  a.content AS answer
+FROM questions q
+JOIN responses r ON q.id = r.question_id
+JOIN choices c ON q.id = c.question_id
+JOIN answers a ON q.id = a.question_id
+WHERE q.quiz_id = ? AND r.submission_id = ?
+GROUP BY q.id
+";
+$stmt = prepare_and_execute($sql, [$submission["quiz_id"], $submission["id"]]);
+$rows = $stmt->fetchAll();
+
+$num_correct = 0;
+
+foreach ($rows as $row) {
+  if ($row["response"] == $row["answer"]) {
+    $num_correct++;
+  }
+}
+
+$num_questions = $stmt->rowCount();
+$grade = round(($num_correct / $num_questions) * 100, 2);
+
+// echo "<pre style='font-size: 9px;'>";
+// print_r($rows);
+// echo "</pre>";
+
 ?>
 
 <!doctype html>
@@ -53,8 +93,41 @@ session_start();
     </div>
   </nav>
 
-  <div class="mx-auto bg-white min-h-screen px-12 pt-4">
+  <div class="mx-auto bg-white min-h-screen px-12 pt-4 w-1/2">
+    <div>
+      <a href="./index.php" class="text-gray-900 hover:text-gray-700">‹ Back</a>
+    </div>
+    <br>
+    <div>
+      name: <?= $submission["submitter"] ?>
+    </div>
+    <div>
+      grade: <?= $grade ?>
+    </div>
 
+    <form method="">
+      <fieldset disabled="disabled">
+        <?php foreach ($rows as $index => $row) : ?>
+          <div class="border rounded-lg my-10 px-4 py-2 border-2 <?= ($row["response"] ==  $row["answer"]) ? "border-green-500" : "border-red-500" ?>">
+            <legend class="text-sm font-semibold leading-6 text-gray-900">Question #<?= $index + 1; ?></legend>
+            <p class="mt-1 text-sm leading-6 text-gray-600"><?= $row["question"] ?></p>
+            <div class="mt-6 space-y-2">
+              <?php $h = "question_" . $row["id"]; ?>
+              <?php foreach (explode("|", $row["choices"]) as $index => $choice) : ?>
+                <div class="flex items-center gap-x-3">
+                  <?php if ($row["response"] == $choice) : ?>
+                    <input id="<?= $h . "_" . $index ?>" name="<?= $h ?>" type="radio" value="<?= $choice ?>" required checked>
+                  <?php else : ?>
+                    <input id="<?= $h . "_" . $index ?>" name="<?= $h ?>" type="radio" value="<?= $choice ?>" required>
+                  <?php endif; ?>
+                  <label for="<?= $h . "_" . $index ?>" class="block text-sm font-medium leading-6 text-gray-900"><?= htmlspecialchars($choice) ?></label>
+                </div>
+              <?php endforeach ?>
+            </div>
+          </div>
+        <?php endforeach; ?>
+        <fieldset>
+    </form>
   </div>
 
 </body>
