15d71dc97110ea690b947f5be701cab1cd6f4917
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-02-28 08:20:15 -0500

    Update edit.php

--- quizzes/edit.php
+++ quizzes/edit.php
@@ -39,6 +39,13 @@ if (isset($_POST["new-mc-question"])) {
   exit;
 }
 
+if (isset($_POST["rename-save-button"])) {
+  $sql = "UPDATE quizzes SET name = ? WHERE id = ? LIMIT 1";
+  prepare_and_execute($sql, [$_POST["new-quiz-name"], $quiz_id]);
+  header("Location: ./edit.php?quiz_id={$_GET["quiz_id"]}");
+  exit;
+}
+
 $sql = "SELECT * FROM quizzes WHERE id = ? LIMIT 1";
 $stmt = prepare_and_execute($sql, [$quiz_id]);
 $quiz = $stmt->fetch();
@@ -214,11 +221,12 @@ $questions = $stmt->fetchAll();
         <button id="show-dialog" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
           + New question
         </button>
-        <button class="rounded-md bg-indigo-300 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+
+        <button id="show-rename-dialog" class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           Rename
         </button>
         <button class="rounded-md bg-indigo-300 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          Clone quiz
+          Clone
         </button>
       </div>
     </div>
@@ -324,6 +332,33 @@ $questions = $stmt->fetchAll();
       </form>
     </dialog>
 
+    <!-- Rename modal -->
+    <dialog class="w-2/5 rounded-xl backdrop:backdrop-brightness-[65%] h-[405px]" id="rename-dialog">
+      <form method="post" class="mt-5 mb-14">
+
+        <div class="space-y-12">
+          <div class="border-b border-gray-900/10 pb-12">
+            <h2 class="text-base font-semibold leading-7 text-gray-900">Rename Quiz</h2>
+            <p class="mt-1 text-sm leading-6 text-gray-600">Update the name of this quiz.</p>
+
+            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div class="sm:col-span-4">
+                <label class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                <div class="mt-2">
+                  <input name="new-quiz-name" type="text" autocomplete="email" class="block px-2.5 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value="<?= $quiz["name"] ?>">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+          <button type="button" class="js-close text-sm font-semibold leading-6 text-gray-900" id="">Cancel</button>
+          <button type="submit" name="rename-save-button" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+        </div>
+      </form>
+    </dialog>
+
     <script>
       function openCity(evt, cityName) {
         var i, tabcontent, tablinks;
@@ -346,6 +381,13 @@ $questions = $stmt->fetchAll();
       const dialog = document.getElementById("dialog");
       const jsCloseBtns = document.getElementsByClassName("js-close");
 
+      const showRenameDialogButton = document.getElementById("show-rename-dialog");
+      const renameDialog = document.getElementById("rename-dialog");
+
+      showRenameDialogButton.addEventListener("click", () => {
+        renameDialog.showModal();
+      })
+
       showBtn.addEventListener("click", () => {
         dialog.showModal();
       });
@@ -354,6 +396,7 @@ $questions = $stmt->fetchAll();
         b.addEventListener("click", (e) => {
           e.preventDefault();
           dialog.close();
+          renameDialog.close();
         });
       }
     </script>
