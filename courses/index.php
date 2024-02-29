99dff8d8c1ffcb1aefff6679ed60a51e31508101
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-02-28 21:03:05 -0500

    Update index.php

--- courses/index.php
+++ courses/index.php
@@ -83,7 +83,7 @@ $courses = $stmt->fetchAll();
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#f87171" aria-hidden="true" class="w-6 h-6">
           <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clip-rule="evenodd"></path>
         </svg>
-        <p class="text-red-700 text-[14px] font-medium px-2">
+        <p class="text-red-700 text-[14px] px-2">
           <?= $_SESSION["error_message"] ?>
         </p>
       </div>
