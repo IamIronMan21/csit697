bb8c8e13cdd7d9db23c4844dbd43f34d422f926b
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-02-29 17:46:19 -0500

    Update edit.php

--- quizzes/edit.php
+++ quizzes/edit.php
@@ -347,7 +347,7 @@ $questions = $stmt->fetchAll();
                     <div class="sm:col-span-4">
                       <label class="block text-sm font-medium leading-6 text-gray-900">Question</label>
                       <div class="mt-2">
-                        <input name="question-<?= $row["id"]; ?>" type="text" autocomplete="email" class="block px-2.5 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value="<?= $row["question"] ?>">
+                        <input name="question-<?= $row["id"]; ?>" type="text" required class="block px-2.5 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value="<?= $row["question"] ?>">
                       </div>
                     </div>
                   </div>
