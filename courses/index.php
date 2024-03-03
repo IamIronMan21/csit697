b77c88af40f16c51264362d28585ecff3e7cc3b9
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-03-03 00:15:57 -0500

    .

--- courses/index.php
+++ courses/index.php
@@ -78,29 +78,19 @@ $courses = $stmt->fetchAll();
 
   <div class="mx-auto bg-white min-h-screen px-12 pt-4">
 
-    <?php if (isset($_SESSION["error_message"])) : ?>
-      <div class="flex items-center bg-red-50 rounded-lg py-3 px-3 border border-red-600 mb-4">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#f87171" aria-hidden="true" class="w-6 h-6">
-          <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clip-rule="evenodd"></path>
-        </svg>
-        <p class="text-red-700 text-[14px] px-2">
-          <?= $_SESSION["error_message"] ?>
-        </p>
-      </div>
-      <?php unset($_SESSION["error_message"]) ?>
-    <?php endif; ?>
+    <?php
+    if (isset($_SESSION["error_message"])) {
+      display_error_message($_SESSION["error_message"]);
+      unset($_SESSION["error_message"]);
+    }
 
-    <?php if (isset($_SESSION["success_message"])) : ?>
-      <div class="flex items-center bg-green-50 rounded-lg py-3 px-3 border border-green-600 mb-4">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#4ade80" aria-hidden="true" class="w-6 h-6">
-          <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd"></path>
-        </svg>
-        <p class="text-green-700 text-[14px] font-medium px-2">
-          <?= $_SESSION['success_message'] ?>
-        </p>
-      </div>
-      <?php unset($_SESSION["success_message"]) ?>
-    <?php endif; ?>
+    ?>
+    <?php
+    if (isset($_SESSION["success_message"])) {
+      display_success_message($_SESSION["success_message"]);
+      unset($_SESSION["success_message"]);
+    }
+    ?>
 
     <div class="flex mb-4 items-center">
       <h1 class="grow text-xl font-medium">Courses</h1>
