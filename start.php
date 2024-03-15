4db95bcc14bf2b302aa769eadb5760e7ee236141
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-03-15 10:49:51 -0400

    Update start.php

--- start.php
+++ start.php
@@ -61,17 +61,24 @@ if (isset($_POST["submit-button"])) {
       <button name="submit-button" type="submit" class="rounded-md w-full bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enter</button>
     </form>
 
-    <a class="text-indigo-600 hover:text-indigo-500" href="./login.php">Sign in as tutor ›</a>
+    <hr class="w-4/5 mx-auto">
 
-    <div class="mt-6">
-      <a href="./index.php" id="" class="flex w-1/4 items-center mx-auto justify-center rounded-md bg-white px- py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-        </svg>
-        <div class="text-center ml-2">
-          Return
-        </div>
-      </a>
+    <div>
+
+      <div class="my-5">
+        <a class="text-indigo-600 hover:text-indigo-500" href="./login.php">Sign in as tutor ›</a>
+      </div>
+
+      <div class="mt-6">
+        <a href="./index.php" id="" class="flex w-1/4 items-center mx-auto justify-center rounded-md bg-white px- py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+          </svg>
+          <div class="text-center ml-2">
+            Return
+          </div>
+        </a>
+      </div>
     </div>
   </div>
 </body>
